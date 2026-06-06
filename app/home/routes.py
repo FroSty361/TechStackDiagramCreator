9a48612290b1d74374d5341551dad524bfaac795
FroSty361 <192673158+FroSty361@users.noreply.github.com>
2026-06-06 01:31:11 -0700

    Added Logging

--- app/home/routes.py
+++ app/home/routes.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
 from . import home
+import datetime as dt
 
 @home.route('/')
 def index():
@@ -7,4 +8,19 @@ def index():
 
 @home.route('/result/', methods=['GET', 'POST'])
 def result():
+    if request.method == 'POST':
+        githubURL = request.form['githubURL']
+
+        print(githubURL)
+
+        return render_template("home/result.html")
+
     return render_template("home/result.html")
+
+@home.route('/logs/')
+def logs_iframe():
+    time = dt.datetime.now()
+
+    timeStr = time.strftime("%H:%M:%S")
+
+    return render_template("home/iframe-logs.html", time=timeStr, logs=["Hi!", "Mesmeizer", "Sakuya Uses Pads"])
