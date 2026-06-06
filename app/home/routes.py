756ba71933bceeab67ef68a4661a0ccd11431fb3
FroSty361 <192673158+FroSty361@users.noreply.github.com>
2026-06-05 23:55:01 -0700

    Added /result/ route

--- app/home/routes.py
+++ app/home/routes.py
@@ -4,3 +4,7 @@ from . import home
 @home.route('/')
 def index():
     return render_template("home/index.html")
+
+@home.route('/result/', methods=['GET', 'POST'])
+def result():
+    return render_template("home/result.html")
